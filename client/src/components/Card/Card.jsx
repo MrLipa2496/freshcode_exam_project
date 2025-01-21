bda2ced89f716a2722734d6fc590f8d7007777fd
MrLipa2496 <sasalipa216@gmail.com>
2025-01-21 16:25:33 +0200

    Optimize component card & styles for it

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
+import classNames from 'classnames';
+import { FaArrowRightLong } from 'react-icons/fa6';
 import styles from './Card.module.sass';
 
-const Card = ({ logo, title, description, buttonText, onButtonClick }) => {
+const Card = ({
+  logo,
+  title,
+  description,
+  buttonText,
+  variant = 'default',
+  isLastStep = false,
+}) => {
+  const cardClass = classNames(styles.card, {
+    [styles.stepsCard]: variant === 'steps',
+  });
+
+  const titleClass = classNames(styles.title, {
+    [styles.stepsTitle]: variant === 'steps',
+  });
+
   return (
-    <div className={styles.card}>
-      <div className={styles.logoContainer}>
-        <img src={logo} alt='logo' />
-      </div>
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.description}>{description}</p>
-      <button className={styles.button} onClick={onButtonClick}>
-        {buttonText}
-      </button>
+    <div className={cardClass}>
+      {logo && (
+        <div className={styles.logoContainer}>
+          <img src={logo} alt='logo' />
+        </div>
+      )}
+      {title && <h2 className={titleClass}>{title}</h2>}
+      {description && <p className={styles.description}>{description}</p>}
+      {variant === 'steps' && !isLastStep && (
+        <FaArrowRightLong className={styles.arrow} />
+      )}
+      {buttonText && (
+        <button className={styles.button}>
+          {buttonText} <FaArrowRightLong className={styles.btnArrow} />
+        </button>
+      )}
     </div>
   );
 };
