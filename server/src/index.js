18bdc195b2a711c50dfae4d30119dec2adbd17ef
MrLipa2496 <sasalipa216@gmail.com>
2025-02-21 21:07:03 +0200

    Add global uncaughtException and unhandledRejection handlers & Add error handling middleware to app

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,8 @@ require('./dbMongo/mongoose');
 const router = require('./router');
 const controller = require('./socketInit');
 const handlerError = require('./handlerError/handler');
+const errorHandler = require('./middlewares/errorHandlerMiddleware');
+const logError = require('./utils/logger');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,6 +18,18 @@ app.use(express.json());
 app.use('/public', express.static('public'));
 app.use(router);
 app.use(handlerError);
+app.use(errorHandler);
+
+process.on('uncaughtException', err => {
+  logError(err);
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  logError(reason);
+  console.error('Unhandled Rejection:', reason);
+});
 
 const server = http.createServer(app);
 server.listen(PORT, () =>
