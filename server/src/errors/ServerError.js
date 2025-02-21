3b7bc19fea670b1f2d69708fa1c7e08aa7bc24f6
MrLipa2496 <sasalipa216@gmail.com>
2025-02-21 21:03:27 +0200

    Add log errors on ServerError

--- server/src/errors/ServerError.js
+++ server/src/errors/ServerError.js
@@ -1,8 +1,16 @@
 const ApplicationError = require('./ApplicationError');
+const logError = require('../utils/logger');
 
 class ServerError extends ApplicationError {
   constructor (message) {
     super(message || 'server error', 500);
+
+    logError({
+      message: this.message,
+      time: Date.now(),
+      code: this.code,
+      stackTrace: this.stack,
+    });
   }
 }
 
